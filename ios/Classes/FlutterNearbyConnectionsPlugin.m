36f846be3e90201ac6bf3d07da4ee33472a739f0
Luis Ibarra <luise98cu@gmail.com>
2024-01-05 10:37:37 -0500

    iOS Fix v2.0

--- ios/Classes/FlutterNearbyConnectionsPlugin.m
+++ ios/Classes/FlutterNearbyConnectionsPlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterNearbyConnectionsPlugin.h"
-#if __has_include(<flutter_nearby_connections/flutter_nearby_connections-Swift.h>)
-#import <flutter_nearby_connections/flutter_nearby_connections-Swift.h>
+#if __has_include(<light_flutter_nearby_connections/light_flutter_nearby_connections-Swift.h>)
+#import <light_flutter_nearby_connections/light_flutter_nearby_connections-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_nearby_connections-Swift.h"
+#import "light_flutter_nearby_connections-Swift.h"
 #endif
 
 @implementation FlutterNearbyConnectionsPlugin
